286eb3f96fe04e1e6cea7fdfaa482b1fa39901ed
Laith Younes <laithyounes@Laiths-MacBook-Pro.local>
2023-05-11 23:08:26 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { GithubIcon, EmailIcon, FacebookIcon, InstagramIcon, TwitterIcon } from './icons';
+
+const socialLinks = [
+  { href: "https://github.com/PepperBot-co", title: "GitHub", icon: <GithubIcon /> },
+  { href: "mailto:hello@pepperbot.co", title: "Email", icon: <EmailIcon /> },
+  { href: "https://facebook.com/pepperbot.co", title: "Facebook", icon: <FacebookIcon /> },
+  { href: "https://www.instagram.com/pepperbot.co", title: "Instagram", icon: <InstagramIcon /> },
+  { href: "https://twitter.com/pepperbot_co", title: "Twitter", icon: <TwitterIcon /> },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <h1>Welcome to Pepperbot!</h1>
+        <p>Pepperbot is your personal AI assistant, designed to make your life easier.</p>
+        <div className="icons-container">
+          {socialLinks.map(({ href, title, icon }) => (
+            <a className={`icon ${title.toLowerCase()}`} target="_blank" rel="noopener noreferrer" href={href} title={title} key={title}>
+              <div className="ir">
+                {icon}
+              </div>
+            </a>
+          ))}
+        </div>
       </header>
     </div>
   );
